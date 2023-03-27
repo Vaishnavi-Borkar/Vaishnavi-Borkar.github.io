f344b7a9bca2a5e0110761cd1b047592d36d7ce9
vaishnavi borkar <105918011+Vaishnavi-Borkar@users.noreply.github.com>
2023-03-27 22:56:02 +0530

    portfolio added

--- src/Component/Projects.jsx
+++ src/Component/Projects.jsx
@@ -18,17 +18,27 @@ import beautybebo from "../Images/beauty-bebo.png";
 import bewakoof from "../Images/bewakoof.png";
 import sastadeal from "../Images/sastadeal.png";
 import urban from "../Images/urban.png";
-import {} from "@chakra-ui/react";
+import { SiRedux, SiNodedotjs, SiExpress, SiChakraui } from "react-icons/si";
+import { FaCss3Alt, FaHtml5, FaReact } from "react-icons/fa";
+import { MdMilitaryTech } from "react-icons/md";
+import { DiMongodb } from "react-icons/di";
+import { SiJavascript } from "react-icons/si";
 
 const Allproject = [
   {
     img: sastadeal,
     title: "Snapdeal-Clone",
     description:
-      "Snapdeal is an Indian e-commerce website Snapdeal was one of the largest online marketplaces in India. Snapdeal targets the value e-commerce segment.It was a Collaborative Project built in 5 days, App was built using React-redux ,HTML,CSS,JavaScript,and Chakra-ui.",
+      "Snapdeal is an Indian e-commerce website Snapdeal was one of the largest online marketplaces in India. Snapdeal targets the value e-commerce segment.It was a Collaborative Project built in 5 days.",
     git: "https://github.com/bhaveshkumar96/Snapdeal.com-Clone",
     link: "https://snapdeal-project.vercel.app/",
-    tech: " CSS | React | Redux | Chakra-Ui  ",
+    // tech: " CSS | React | Redux | Chakra-Ui  ",
+    tech: [
+      <FaCss3Alt size={"44px"} />,
+      <FaReact size={"44px"} />,
+      <SiRedux size={"44px"} />,
+      <SiChakraui size={"44px"} />,
+    ],
   },
   {
     img: beautybebo,
@@ -37,7 +47,12 @@ const Allproject = [
       "Beauty Bebo is India’s fastest growing online retail store for the day to day and special occasion need of the Makeup, Skin care, Hair care, Personal care, Mom and Baby care, Fragrance and Ayurveda products. ",
     git: "https://github.com/lokesh-dc/Beauty-Bebo-Clone",
     link: "https://bright-puppy-c195ee.netlify.app/",
-    tech: "HTML | CSS | JavaScript  ",
+    tech: [
+      <FaCss3Alt size={"44px"} />,
+      <FaReact size={"44px"} />,
+      <SiJavascript size={"44px"} />,
+      <FaHtml5 size={"44px"} />,
+    ],
   },
   {
     img: bewakoof,
@@ -47,7 +62,13 @@ const Allproject = [
 
     git: "https://github.com/akaushik509/brainy-deer-5360",
     link: "https://trendy-treasures.netlify.app/",
-    tech: " CSS | React | Mongodb | Chakra-Ui ",
+    // tech: " CSS | React | Mongodb | Chakra-Ui ",
+    tech: [
+      <FaCss3Alt size={"44px"} />,
+      <FaReact size={"44px"} />,
+      <DiMongodb size={"44px"} />,
+      <SiChakraui size={"44px"} />,
+    ],
   },
   {
     img: urban,
@@ -56,7 +77,13 @@ const Allproject = [
       "Urban Company is your one-stop destination for expert local services. Get dozens of trusted professionals near you to take care of all your home",
     git: "https://github.com/Vaishnavi-Borkar/UrbanCompany",
     link: "https://helpinghand-gamma.vercel.app/",
-    tech: "HTML | CSS | React | Chakra-Ui",
+    // tech: "HTML | CSS | React | Chakra-Ui",
+    tech: [
+      <FaHtml5 size={"44px"} />,
+      <FaCss3Alt size={"44px"} />,
+      <FaReact size={"44px"} />,
+      <SiChakraui size={"44px"} />,
+    ],
   },
 ];
 
@@ -89,20 +116,20 @@ function Projects() {
             boxShadow={"rgba(0, 0, 0, 0.24) 0px 3px 8px"}
             key={title}
             // w={"500px"}
-            m={"50px"}
+            // m={"50px"}
             w={{ base: "250px", sm: "300px", md: "300px", lg: "400px" }}
             aline={"center"}
             justify={"center"}
             margin={"auto"}
           >
             <Image
-              padding={7}
+              padding="10px"
               boxShadow={
                 "rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px"
               }
               src={img}
               alt={title}
-              w={{ base: "200px", sm: "250px", md: "280px" }}
+              w={{ base: "200px", sm: "250px", md: "280px", lg: "350px" }}
             />
             <Text color={"#f09e06 "} fontSize="3xl" mt="100px">
               {title}
@@ -115,9 +142,12 @@ function Projects() {
             >
               {description}
             </Text>
-            <Text frontWidth={"lg"} color={"#f09e06"} fontWeight={"bold"}>
-              Tech stack: {tech}
-            </Text>
+            <HStack color={"#f09e06"}>
+              <Box>{tech[0]}</Box>
+              <Box>{tech[1]}</Box>
+              <Box>{tech[2]}</Box>
+              <Box>{tech[3]}</Box>
+            </HStack>
             <HStack padding={7}>
               <Link
                 boxShadow={
